4d87a5566c8c261a84670f1bd257653951dddd77
Natalia Lazar <natalia.andreea.lazar@gmail.com>
2019-02-22 01:28:08 +0200

    extract generic method for creating xml tags and setting the amount for the leaf tag

--- invoice_processor.py
+++ invoice_processor.py
@@ -23,11 +23,15 @@ class InvoiceXMLProcessor:
         invoice_file_name = basename(input_file_path)
         return join(output_directory.strip(), invoice_file_name)
 
-    def __add_or_set_amount_element(self, item, amount):
-        amount_element = item.find(self.amount.text)
-        if amount_element is None:
-            amount_element = ElementTree.SubElement(item, self.amount)
-        amount_element.text = '{:.2f}'.format(amount)
+    def _update_or_add_amount(self, item, relative_path, amount):
+        element_for_path = item.find(relative_path)
+        if element_for_path is None:
+            split_path = relative_path.split('/')
+            # first path chunk is the current existing item and the second chunk is the one we need to handle
+            path_to_create = split_path.pop(1)
+            element_for_path = ElementTree.SubElement(item, path_to_create)
+            self._update_or_add_amount(element_for_path, "/".join(split_path), amount)
+        element_for_path.text = '{:.2f}'.format(amount)
 
     def __fix_row_amount(self, item):
         quantity = item.find(self.quantity.text)
@@ -35,25 +39,11 @@ class InvoiceXMLProcessor:
         if quantity is None or price is None:
             raise BadXMLFormat("Missing quantity or price")
         amount = float(quantity.text) * float(price.text)
-        self.__add_or_set_amount_element(item, amount)
+        self._update_or_add_amount(item, './{}'.format(self.amount), amount)
         return amount
-
-    def __add_totals_element(self):
-        totals_element = self.root.find(self.totals.text)
-        if not totals_element:
-            totals_element = ElementTree.SubElement(self.root, self.totals)
-        self.__add_or_set_amount_element(totals_element, self.total_sum)
-
-    def __update_or_add_total_amount(self):
-        total_amount_element = self.root.find('./{}/{}'.format(self.totals, self.amount))
-        if total_amount_element is None:
-            self.__add_totals_element()
-        else:
-            total_amount_element.text = '{:.2f}'.format(self.total_sum)
 
     def fix_invoice_amounts(self):
         for row in self.root.findall(self.row.text):
             self.total_sum += self.__fix_row_amount(row)
-
-        self.__update_or_add_total_amount()
+        self._update_or_add_amount(self.root, './{}/{}'.format(self.totals, self.amount), self.total_sum)
         self.dom.write(self.output_file_path)
